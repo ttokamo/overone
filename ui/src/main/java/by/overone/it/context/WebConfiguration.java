fc137c4319de0ca6dafbbd6b017f4edf4b59e2aa
tokamo <worcers@gmail.com>
2022-01-24 21:08:27 +0300

    add spring mvc configurations

---
+++ ui/src/main/java/by/overone/it/context/WebConfiguration.java
@@ -0,0 +1,22 @@
+package by.overone.it.context;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = {"by.overone.it", "by.overone.it.pojo", "by.overone.it.dao", "by.overone.it.controllers"})
+@EnableWebMvc
+public class WebConfiguration {
+
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver resolver =
+                new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
