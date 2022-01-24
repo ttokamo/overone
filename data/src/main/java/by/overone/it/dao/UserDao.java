f9253bccdad2d532d1320a83450bcbd47af0d7b1
tokamo <worcers@gmail.com>
2022-01-25 00:58:45 +0300

    add method "getUserByEmail"

--- data/src/main/java/by/overone/it/dao/UserDao.java
+++ data/src/main/java/by/overone/it/dao/UserDao.java
@@ -3,12 +3,14 @@ package by.overone.it.dao;
 import by.overone.it.pojo.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.io.Serializable;
+import java.util.List;
 
 @Transactional
 @Repository
@@ -22,13 +24,16 @@ public class UserDao {
     public Serializable addUser(User user) {
         Session session = sessionFactory.getCurrentSession();
         Serializable id = null;
-
-        try {
-            id = session.save(user);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        id = session.save(user);
         return id;
+    }
+
+    public List<User> getUserByEmail(String email) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<User> query = session.createQuery("" +
+                "from User where email =: paramEmail", User.class
+        );
+        query.setParameter("paramEmail", email);
+        return query.list();
     }
 }
