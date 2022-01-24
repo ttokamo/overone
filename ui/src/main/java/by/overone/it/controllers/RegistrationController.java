2f6561267103a93d27bd51e7c8e91e0926fa08aa
tokamo <worcers@gmail.com>
2022-01-24 21:12:58 +0300

    create controller with method "showLoginPage"

---
+++ ui/src/main/java/by/overone/it/controllers/RegistrationController.java
@@ -0,0 +1,22 @@
+package by.overone.it.controllers;
+
+import by.overon.it.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegistrationController {
+
+    private final UserService userService;
+
+    @Autowired
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "loginPage";
+    }
+}
