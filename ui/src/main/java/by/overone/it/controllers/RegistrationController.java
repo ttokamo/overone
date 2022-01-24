276d874b34111a65878660df5f97ac118bb72daa
tokamo <worcers@gmail.com>
2022-01-25 00:38:04 +0300

    add new method at registration controller

--- ui/src/main/java/by/overone/it/controllers/RegistrationController.java
+++ ui/src/main/java/by/overone/it/controllers/RegistrationController.java
@@ -1,22 +1,36 @@
 package by.overone.it.controllers;
 
-import by.overon.it.service.UserService;
+import by.overone.it.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RegistrationController {
 
-    private final UserService userService;
+    private UserService userService;
 
     @Autowired
     public RegistrationController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping(value = "/")
+    @PostMapping(value = "/finish-registration")
+    public String checkInputData(
+            @RequestParam("firstName") String firstName,
+            @RequestParam("secondName") String secondName,
+            @RequestParam("email") String email,
+            @RequestParam("address") String address,
+            @RequestParam("telephone") String telephone,
+            @RequestParam("password") String password
+    ){
+        userService.addUser(firstName, secondName, email, address, telephone, password);
+        return "redirect:/login";
+    }
+
+    @GetMapping(value = "/registration")
     public String showLoginPage() {
         return "register-page";
     }
